d0d83541addaa5f9637efabdb9e57612d73f9b66
Ranjan Goyal <ranjangoyal98@gmail.com>
2022-02-10 16:02:52 -0500

    Add Product Model and Mockup Data

---
+++ frontend/lib/screens/orders/orders.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Orders extends StatelessWidget {
+  const Orders({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Orders')),
+      body: const Text('Hello'),
+    );
+  }
+}
---
+++ frontend/lib/shared/products_data.dart
@@ -0,0 +1,44 @@
+import '../models/product.dart';
+
+const productsData = [
+  Product(
+    pid: 1,
+    pName: 'Apple Airpods',
+    price: 129,
+    details:
+        "Universal fit that’s comfortable all day, Automatically on, automatically connected, Easy setup for all your Apple devices, Quick access to Siri by saying Hey Siri",
+    brandName: "Apple",
+    merchantName: "Apple Inc.",
+    imgUrl: "https://m.media-amazon.com/images/I/7120GgUKj3L._AC_SL1500_.jpg",
+  ),
+  Product(
+    pid: 2,
+    pName: 'Portable Heater',
+    price: 51,
+    details:
+        "Oscillating Portable Ceramic Space Heater, 750/1500W Electric Heater with Thermostat Overheat Protection and Tip Over Protection, Personal Heater with Carrying Handle for Home Office",
+    brandName: "Apple",
+    merchantName: "Apple Inc.",
+    imgUrl: "https://m.media-amazon.com/images/I/71L-cpRT8IL._AC_SL1500_.jpg",
+  ),
+  Product(
+    pid: 3,
+    pName: 'Office Chair',
+    price: 87,
+    details:
+        "BestOffice Office Chair Desk Gaming Racing High Back Computer Task Swivel Executive Stool with Lumbar Support, Black",
+    brandName: "BestOffice",
+    merchantName: "Cavalier Wholesale",
+    imgUrl: "https://m.media-amazon.com/images/I/61UmfkXCncL._AC_SL1500_.jpg",
+  ),
+  Product(
+    pid: 4,
+    pName: 'Macbook Pro 256 GB',
+    price: 1649,
+    details:
+        "2020 Apple MacBook Pro with Apple M1 Chip (13-inch, 8GB RAM, 256GB SSD Storage) - Space Gray",
+    brandName: "Apple",
+    merchantName: "Apple Inc",
+    imgUrl: "https://m.media-amazon.com/images/I/71an9eiBxpL._AC_SL1500_.jpg",
+  ),
+];
