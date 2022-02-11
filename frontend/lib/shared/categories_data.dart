9fe3d5950b29d96334a7506a415450def866c23a
Ranjan Goyal <ranjangoyal98@gmail.com>
2022-02-10 20:27:02 -0500

    Added Category Mock Data

---
+++ frontend/lib/shared/categories_data.dart
@@ -0,0 +1,34 @@
+import '../models/category.dart';
+
+const productsData = [
+  Category(
+      cid: 1,
+      cName: 'Clothing and Shoes',
+      imgUrl:
+          "https://m.media-amazon.com/images/I/81ha5gEVJkL._AC_UL640_FMwebp_QL65_.jpg"),
+  Category(
+      cid: 1,
+      cName: 'Laptops and Mobiles',
+      imgUrl:
+          "https://m.media-amazon.com/images/I/71an9eiBxpL._AC_UL640_FMwebp_QL65_.jpg"),
+  Category(
+      cid: 1,
+      cName: 'Books and Stationary',
+      imgUrl:
+          "https://m.media-amazon.com/images/I/71WC3z4Wt0L._AC_UL640_FMwebp_QL65_.jpg"),
+  Category(
+      cid: 1,
+      cName: 'Electronics and Accessories',
+      imgUrl:
+          "https://m.media-amazon.com/images/I/71KGrESMM3L._AC_UL640_FMwebp_QL65_.jpg"),
+  Category(
+      cid: 1,
+      cName: 'Home and Kitchen',
+      imgUrl:
+          "https://m.media-amazon.com/images/I/717CGms8KWL._AC_UL640_QL65_.jpg"),
+  Category(
+      cid: 1,
+      cName: 'Beauty and Essentials',
+      imgUrl:
+          "https://m.media-amazon.com/images/I/61tjqD0ODiL._AC_UL640_FMwebp_QL65_.jpg"),
+];
