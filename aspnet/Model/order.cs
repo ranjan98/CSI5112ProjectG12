4d794b2a723ce6907696e598751246680afc6d33
John Jegede <jegede400@gmail.com>
2022-03-26 17:58:01 -0400

    added bson for orders model

--- aspnet/Model/order.cs
+++ aspnet/Model/order.cs
@@ -2,12 +2,22 @@ namespace aspnet.models;
 
 public class Order
 {
+    [BsonId][BsonRepresentation(BsonType.ObjectId)]  
     public string id { get; set; }
+
+    [BsonElement]
     public string invoicenumber { get; set; }
+
+    [BsonElement]
     public string userid { get; set; }
+
+    [BsonElement]
     public double amount { get; set; }
 
+    [BsonElement]
     public List<CartItem> items { get; set; }
+
+    [BsonElement]
     public string datetime { get; set; }
 
     public Order(string Id, string Invoicenumber, string Userid, double Amount, List<CartItem> Items, string Datetime)
