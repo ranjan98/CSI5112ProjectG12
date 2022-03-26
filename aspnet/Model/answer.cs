3719a37d28c5eebce703e230c228126ff3379116
John Jegede <jegede400@gmail.com>
2022-03-26 17:57:00 -0400

    Added bson for order,category and answers model

--- aspnet/Model/answer.cs
+++ aspnet/Model/answer.cs
@@ -2,10 +2,19 @@ namespace aspnet.models;
 
 public class Answer
 {
+    [BsonId][BsonRepresentation(BsonType.ObjectId)] 
     public string id { get; set; }
+
+    [BsonElement]
     public string reply { get; set; }
+
+    [BsonElement]
     public string uid { get; set; }
+
+    [BsonElement]
     public string datetime { get; set; }
+
+    [BsonElement]
     public string qid { get; set; }
 
     public Answer(string Id, string Reply, string Uid, string Datetime, string Qid)
--- aspnet/Model/category.cs
+++ aspnet/Model/category.cs
@@ -2,8 +2,13 @@ namespace aspnet.models;
 
 public class Category
 {
+    [BsonId][BsonRepresentation(BsonType.ObjectId)] 
     public string cid { get; set; }
+
+    [BsonElement]
     public string cName { get; set; }
+
+    [BsonElement]
     public string imgUrl { get; set; }
 
     public Category(string Cid, string CName, string ImgUrl)
