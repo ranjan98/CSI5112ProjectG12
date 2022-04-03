dafc9ff338ea51c9e72106588c20efd966fedd3f
Ranjan Goyal <ranjangoyal98@gmail.com>
2022-04-03 16:01:38 -0400

    Removing options

--- aspnet/Program.cs
+++ aspnet/Program.cs
@@ -29,7 +29,7 @@ builder.Services.Configure<uomartDatabaseSettings>(
 // Adding services for Direct Injection
 
 var options = builder.Configuration.GetSection(nameof(uomartDatabaseSettings)).Get<uomartDatabaseSettings>();
-builder.Services.AddSingleton<uomartDatabaseSettings>(options);
+builder.Services.AddSingleton<uomartDatabaseSettings>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
